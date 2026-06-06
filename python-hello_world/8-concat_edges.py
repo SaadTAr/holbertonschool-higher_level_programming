adb106df8aa8bf54036b111b8427b2e66486807d
SaadTAr <saadalatar3445@gmail.com>
2026-06-06 18:36:35 +0000

    Fix task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-str = "Python"
-str = str + " is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[-6:])
+word = "Python"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("object-oriented programming with {}{}".format(word, ""))
