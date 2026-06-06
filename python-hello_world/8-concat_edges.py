51b7aa86088684f95ab0fab175d89f1158eaa284
SaadTAr <saadalatar3445@gmail.com>
2026-06-06 19:55:06 +0000

    Task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 word = "Python"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:4]
-print("object-oriented programming with {}{}".format(word_first_3, word_last_2))
+print("Object-oriented programming with {}{}{}".format(word[:3], word[-2:], word[3:-2]))
