f300a2ba7a8db5070a49c1d94298d7c7334bfa5f
SaadTAr <saadalatar3445@gmail.com>
2026-06-13 21:18:02 +0000

    Add text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Text indentation module."""
+
+
+def text_indentation(text):
+    """Print text with 2 new lines after ., ? and :."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    chars = [".", "?", ":"]
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in chars:
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
