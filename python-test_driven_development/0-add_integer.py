ba12171d1af178f8b1d754af02b36f8c561fad4c
SaadTAr <saadalatar3445@gmail.com>
2026-06-13 20:35:06 +0000

    Fix float overflow

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,17 @@
 
 def add_integer(a, b=98):
     """Add two integers"""
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if a == float("inf") or a == float("-inf"):
+        raise OverflowError("cannot convert float infinity to integer")
+
+    if b == float("inf") or b == float("-inf"):
+        raise OverflowError("cannot convert float infinity to integer")
 
     return int(a) + int(b)
