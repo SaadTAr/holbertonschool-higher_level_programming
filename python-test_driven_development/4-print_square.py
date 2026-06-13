99af33a37ce8c3df70d15952d4915a7f309747b8
SaadTAr <saadalatar3445@gmail.com>
2026-06-13 21:03:37 +0000

    Fix print_square docs

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""Module that prints a square."""
+
+
 def print_square(size):
-    if not isinstance(size, int):
+    """Print a square with the character #."""
+
+    if type(size) is float and size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) is not int and type(size) is not float:
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
+
+    size = int(size)
 
     for i in range(size):
         print("#" * size)
