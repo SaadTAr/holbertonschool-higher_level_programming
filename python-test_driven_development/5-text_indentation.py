8fc90d8355af7aefce5ea4bdfd543b85314d0a47
SaadTAr <saadalatar3445@gmail.com>
2026-06-13 21:48:30 +0000

    Fix text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,20 +3,19 @@
 
 
 def text_indentation(text):
-    """Print text with 2 new lines after ., ? and :."""
+    """Print text with two new lines after '.', '?' and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
+    line = ""
 
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print("\n")
+    for char in text:
+        line += char
 
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
+        if char in ".?:":
+            print(line.strip())
+            print()
+            line = ""
 
-        i += 1
+    if line.strip():
+        print(line.strip(), end="")
