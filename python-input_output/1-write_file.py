30ec4d2f7641c80244f7b24b83aa7376c27bebd1
SaadTAr <saadalatar3445@gmail.com>
2026-07-04 19:41:56 +0000

    Fix PEP8

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Write file module"""
+"""Write file module."""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return the number of characters written."""
+    """
+    Write a string to a UTF-8 text file.
+
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
