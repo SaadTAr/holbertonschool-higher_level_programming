0de561e8eff194fabdcf4551a07103cbe7c0031b
SaadTAr <saadalatar3445@gmail.com>
2026-07-28 16:00:16 +0000

    Add City model and list cities by state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """Represents the cities table."""
+
+    __tablename__ = "cities"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
+
+    state = relationship("State", back_populates="cities")
